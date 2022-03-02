d96b0308f85d14c28a4ddc57bb856b90e923ec4f
Jake Eissler <jakeeissler1@gmail.com>
2022-03-01 21:54:40 -0700

    added additional cards for my first project and two exemplary homework assignments. I am leaving the JS alone for now while I determine the ultimate route I want to go with adding these items to my page.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-console.log("yooo");
+//WORK IN PROGRESS
+//var declarations
+// const displayProjList = document.getElementById("display-project-list");
+// const projList = document.getElementById("project-list");
+// const projects = document.createElement("li");
+
+// //click function to show list of projects
+// displayProjList.addEventListener("click", function () {
+//   projList.appendChild(projects);
+//   projects.textContent = "Pokedex";
+// });
+// COMMENT LINE 3-11 TO RESUME
